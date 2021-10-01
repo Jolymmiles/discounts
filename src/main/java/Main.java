d7779ff7c9c93e5bf4c63f5568b81290aad621b4
Валентин <valmak04@gmail.com>
2021-10-01 17:58:05 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //write your code here
+        Scanner sc = new Scanner(System.in);
+        double cost = 0, cost_not_percent = 0, sum_pecent = 0;
+        while (true) {
+            double input_digit = sc.nextDouble();
+            if (input_digit < 0 ) break;
+            if (input_digit >= 1000) {
+                cost += input_digit - input_digit * 0.05;
+                sum_pecent += input_digit * 0.05;
+                cost_not_percent += input_digit;
+            } else {
+                cost += input_digit;
+                cost_not_percent += input_digit;
+            }
+
+        }
+        System.out.print(Math.floor(cost_not_percent*100)/100 + " " + Math.floor(cost*100)/100 + " " + Math.floor(sum_pecent*100)/100);
     }
 }
